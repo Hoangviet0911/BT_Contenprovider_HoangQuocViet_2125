b439531ad227d38b6a07c404ef1455b989cd1045
Hoang Quoc Viet <viethoangquoc2004@gmail.com>
2024-11-03 16:27:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/contenprovider_contacs/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.contenprovider_contacs;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CONTACT_ASK_PERMISSION = 1001;
+    private static final int REQUEST_SMS_ASK_PERMISSION = 1002;
+    Button btn1, btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addControl();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xulyMoDanhba();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xulyMoTinnhan();
+            }
+        });
+    }
+
+    private void xulyMoDanhba() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, REQUEST_CONTACT_ASK_PERMISSION);
+        } else {
+            Intent intent = new Intent(MainActivity.this, Danhba.class);
+            startActivity(intent);
+        }
+    }
+
+    private void xulyMoTinnhan() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, REQUEST_SMS_ASK_PERMISSION);
+        } else {
+            Intent intent = new Intent(MainActivity.this, Doctinnhan.class);
+            startActivity(intent);
+        }
+    }
+
+    private void addControl() {
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+    }
+}
